2c48367dc1778a7a1204eefa667b742d00db34ce
santomatthew <matthewalesandro78@gmail.com>
2021-12-05 21:35:44 +0700

    update untuk jsonwebtoken

--- controller/accountController.js
+++ controller/accountController.js
@@ -3,6 +3,8 @@ const routes = express.Router()
 const eCommAcc = require('../service/account')
 
 // api account
+routes.post('/getAccount',eCommAcc.verifyToken, eCommAcc.getAccountWithToken)
+routes.get('/getAllAcount',eCommAcc.getAllListAccout)
 routes.post('/createAccount',eCommAcc.createAccount)
 routes.delete('/deleteAccount/:id',eCommAcc.deleteAccount)
 routes.post('/loginAccount',eCommAcc.loginAccount)
